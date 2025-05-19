2004872bc6f08f3c935cb03040a9c48418c95cca
hyunil-cho <jackandjills@naver.com>
2025-05-20 00:30:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from project.manager import start
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    options = {
+        "excel" : {},
+        "calendar" : {},
+        "api" : {}
+    }
+    start(options)
+
