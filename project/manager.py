93cb2d01647f224bb0c1adc33d4a5bea39b477c1
hyunil-cho <jackandjills@naver.com>
2025-05-23 23:14:35 +0900

    설명을 위한 주석 부분 추가

--- project/manager.py
+++ project/manager.py
@@ -1,6 +1,6 @@
 from .fetcher.fetchers import SpreadSheetFetcher, GoogleCalendarFetcher, APIServerFetcher
 
-
+# 프로그램 시작 함수. 해당 파일에서는 기존에 만든 컴포넌트를 모아서 조립한 후, 조절하여 실행하는 기능을 담당
 def start(options:dict={}):
 
     fetchers = [SpreadSheetFetcher(options['excel']), GoogleCalendarFetcher(options['calendar']), APIServerFetcher(options['api'])]
