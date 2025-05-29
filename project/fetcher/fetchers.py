b02e7480b1b19ee8195dc2bdc0ac693055f41c91
hyunil-cho <jackandjills@naver.com>
2025-05-29 10:24:52 +0900

    excel에서 데이터 가져와서 처리하는 로직 추가

--- project/fetcher/fetchers.py
+++ project/fetcher/fetchers.py
@@ -1,12 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # 1. 현재 날짜를 구한다.
 # 2. 현재 날짜에 해당하는 행을 구한다
 # 3. 이 중, 양수인 값만 가지고 집계한다
 # 4. 집계 후, 총 건수(양수인 것만) + 총 합계(양수인 것만)
 def get_data_from_excel(url, params={}):
-    pass
+    data_path = './sample_data/membership.xlsx'
+    df = pd.read_excel(data_path, skiprows=1)
+    
+    매출정보_idx = ["일자", "멤버쉽", "이름", "언어", "수단", "코멘트"]
+    멤버쉽_idx = ["타입", "금액.1"]
+    매출정보_df = df[매출정보_idx]
+    
+    멤버쉽_df = df[멤버쉽_idx]
+    
+    df = pd.merge(매출정보_df, 멤버쉽_df, left_on='멤버쉽', right_on='타입').drop('타입', axis=1)
+    
+    today = '2020.05.01'
+    
+    filtered = df[df["일자"]==today]
+    
+    
+    total_sum = filtered['금액.1'].sum()
+    print(total_sum)
 
 def get_data_from_server(url, params={}):
     response = requests.post(url, data=params)
